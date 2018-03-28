d5895235b0a2f33b13d4f08e9965ff96c9a5d6c9
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 18:05:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ def line(current_line)
     line = []
      i = 0
     while i < current_line.length do
-      line.push ("#{i+1}. #{current_line}")
+      line.push ("#{i+1}. #{current_line.join}")
       i+=1
     end
-    puts "The line is currently: #{line.join(" ")}"
+    puts "The line is currently: #{line.join}"
   else
     puts "The line is currently empty."
   end
