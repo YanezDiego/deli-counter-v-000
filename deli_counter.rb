0c0eb9fe17d0a79f36ef75676e79ccfe8c422ec7
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 18:15:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 def line(current_line)
   if 0 < current_line.length
-    line = []
-     i = 0
+    line = [] #created an empty array to push the information into it.
+     i = 0 #Assinged an empty interger to use for the loop.
     while i < current_line.length do
-      line.push ("#{i+1}. #{current_line[i]}") #Created a loop to pass into a new array containing the right position with the correct name.
-      i+=1 #Adding the number & postion by one at the same time.
+      line.push ("#{i+1}. #{current_line[i]}")  #Created a loop to pass into a new array containing the right position with the correct name.
+      i+=1  #Adding the index for the array plus the postion that i want to display by one at the same time.
     end
-    puts "The line is currently: #{line.join(" ")}" #.joined the array into string with one space " " between them.
+    puts "The line is currently: #{line.join(" ")}"  #.joined the array into string with one space " " between them.
   else
     puts "The line is currently empty."
   end
