e0b5e14a05dc68b056be3001f1a2144b12d3ba96
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 16:32:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,16 @@
 # Write your code here.
 def line(katz_deli)
+     if katz_deli.length == 0
+       puts "The line is currently empty."
+     end
+     current_line
   current_line = []
   counter = 1
   katz_deli.each do |name|
     current_line.push("The line is currently: #{counter}. #{name}")
     counter+=1
   end
-  return current_line
+
 #    if katz_deli.length == 0
 #      puts "The line is currently empty."
 #    end
