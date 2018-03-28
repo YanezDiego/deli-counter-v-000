c023a876f800369b8b0cd8e438b2a19bb1f7bf9c
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 19:30:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 def now_serving(current_line)
   if 0 < current_line.length
       while current_line.each != 0 do
-      puts "Currently serving #{current_line.shift}."
+      puts "Currently serving #{current_line}."
     end
   else
     puts "There is nobody waiting to be served!"
