508edb65218510393c208b8702310d0cbb78956e
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 19:51:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 def now_serving(current_line)
   if 0 < current_line.length
     count = 0
-      while current_line.length == i do
+      while current_line.length-1 do
         line.push("#{current_line.shift(i)}")
         i+=1
       end
