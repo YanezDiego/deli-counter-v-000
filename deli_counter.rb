3c0c4a4709d18bc1a90d5ae5ce711566fb5f404b
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 16:25:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,4 +4,8 @@ def line(katz_deli)
   counter = 1
   katz_deli.each do |name|
     current_line.push("The line is currently: #{counter}. #{name}")
+    counter+=1
+  end
+  if katz_deli.lenth == 0
+    puts "The line is currently empty."
 end
