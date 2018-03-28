64f8f6d716095f3640f3f8959f77cd8525613db0
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 18:09:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(current_line)
     line = []
      i = 0
     while i < current_line.length do
-      line.push ("#{i+1}. #{current_line[i]}") #created a loop to pass into a new array containing the right position with the correct name.
-      i+=1
+      line.push ("#{i+1}. #{current_line[i]}") #Created a loop to pass into a new array containing the right position with the correct name.
+      i+=1 #Adding the number & postion by one at the same time.
     end
     puts "The line is currently: #{line.join(" ")}"
   else
