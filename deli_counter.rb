a51f397c4bf8d539ca538c8cd245d43c2c8ab11b
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 18:03:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(current_line)
     line = []
      i = 0
     while i < current_line.length do
-      line.push ("#{i}. #{current_line}")
+      line.push ("#{i+1}. #{current_line}")
       i+=1
     end
     puts "The line is currently: #{line}"
