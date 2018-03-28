9e28e2a6afd0b3df1f423029fabd70d9fac8192a
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 16:55:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(current_line)
   if 0 < current_line.length
-    puts "The line is currently: #{current_line.length}"
+    puts "The line is currently: #{current_line}"
   else
     puts "The line is currently empty."
   end
