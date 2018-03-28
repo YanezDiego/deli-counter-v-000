b86ce47073c83da9ffd0ca927d522461560b6e36
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 18:11:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(current_line)
       line.push ("#{i+1}. #{current_line[i]}") #Created a loop to pass into a new array containing the right position with the correct name.
       i+=1 #Adding the number & postion by one at the same time.
     end
-    puts "The line is currently: #{line}" #.joined the ar
+    puts "The line is currently: #{line.join}" #.joined the ar
   else
     puts "The line is currently empty."
   end
