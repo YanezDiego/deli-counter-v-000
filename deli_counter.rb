21c979ac06d636c4b4c5d7d84873311e53c7c19c
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 16:38:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,3 +2,22 @@
 def line(current_line)
 
 end
+
+
+
+
+
+
+
+
+
+
+#if current_line.length == 0
+#  puts "The line is currently empty."
+#end
+#line_is = []
+#counter = 1
+#current_line.each do |name|
+#line_is.push("The line is currently: #{counter}. #{name}")
+#counter+=1
+#end
